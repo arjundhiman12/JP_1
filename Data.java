773719db26dc42f6ae4e79e62024468b71bfbda0
arjundhiman12 <arjundhiman1221@gmail.com>
2024-09-06 13:54:31 +0530

    Create Data.java

---
+++ Data.java
@@ -0,0 +1,8 @@
+// This is a simple Java program that prints "Hello, World!" to the console.
+
+public class Data {
+    public static void main(String[] args) {
+        // Print the string to the console
+        System.out.println("Hello, World!");
+    }
+}
